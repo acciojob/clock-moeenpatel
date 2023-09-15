5f384ebbb2c892c40a16a328df8334d786134078
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-15 16:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+ function updateClock() {
+            const timerElement = document.getElementById("timer");
+            const now = new Date();
+
+            const options = {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                timeZoneName: 'short'
+            };
+
+            const formattedDate = now.toLocaleDateString(undefined, options);
+            timerElement.textContent = formattedDate;
+        }
+
+        
+        setInterval(updateClock, 1000);
+
+        updateClock();
